52a0018ea40952a01dd8e5a1fea0b473c19940ee
AshviniSutar <83945988+AshviniSutar@users.noreply.github.com>
2021-09-01 18:07:46 +0530

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,14 @@
+
+        burger = document.querySelector('.icon');
+        navbar = document.querySelector('.nav');
+        ul = document.querySelector('.v');
+      
+    
+        
+        burger.addEventListener('click',function(){
+            navbar.classList.toggle('h-nav-resp');
+            ul.classList.toggle('v-class-resp');
+        });
+        
+
+   
